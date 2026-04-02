89af2e027fbc652b972c4614f83ce8ff82491d7d
priyamm704 <134994495+priyamm704@users.noreply.github.com>
2026-04-02 14:28:20 +0530

    Time: 18 ms (52.68%), Space: 82.7 MB (55.44%) - LeetHub

--- 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.java
+++ 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.java
@@ -1,56 +1,56 @@
 class Solution {
     public int minDays(int[] bloomDay, int m, int k) {
         int n = bloomDay.length;
-        if((long)m*k > n)return -1;
+
+        if((long)m * k > n) return -1;
 
         int low = getMin(bloomDay);
         int high = getMax(bloomDay);
 
         while(low <= high){
-            int mid = low + (high-low) / 2;
+            int mid = low + (high - low) / 2;
 
-            if(possible(bloomDay, mid,m, k)){
-                high = mid - 1;
-            }else{
-                low = mid+1;
+            if(possible(bloomDay, mid, m, k)){
+                high = mid-1;
+            } else {
+                low = mid + 1;
             }
         }
         return low;
     }
 
     public static int getMin(int[] arr){
-        int min = Integer.MAX_VALUE;
-        for(int i=0;i<arr.length;i++){
-            if(arr[i] < min){
-                min = arr[i];
-            }
+        int min = arr[0];
+        for(int i = 1; i < arr.length; i++){
+            min = Math.min(min, arr[i]);
         }
         return min;
     }
     
     public static int getMax(int[] arr){
-        int max = Integer.MIN_VALUE;
-        for(int i=0;i<arr.length;i++){
-            if(arr[i] > max){
-                max = arr[i];
-            }
+        int max = arr[0];
+        for(int i = 1; i < arr.length; i++){
+            max = Math.max(max, arr[i]);
         }
         return max;
     }
 
-    public static boolean possible(int[] arr, int mid, int m, int k){
+    public static boolean possible(int[] arr, int day, int m, int k){
         int bouquets = 0;
         int count = 0;
-        for(int i=0;i<arr.length;i++){
-            if(arr[i] <= mid){
+
+        for(int i = 0; i < arr.length; i++){
+            if(arr[i] <= day){
                 count++;
-            }else{
-                bouquets += count/k;
+                if(count == k){
+                    bouquets++;
+                    count = 0;
+                    if(bouquets >= m) return true;
+                }
+            } else {
                 count = 0;
             }
         }
-        bouquets += count/k;
-        if(bouquets >= m) return true;
         return false;
     }
 }
