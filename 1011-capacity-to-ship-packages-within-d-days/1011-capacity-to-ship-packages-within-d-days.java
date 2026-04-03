64d9e2b8d83fa671e78cc8aaa898cea264972a24
priyamm704 <134994495+priyamm704@users.noreply.github.com>
2026-04-03 10:12:05 +0530

    Time: 11 ms (70.29%), Space: 50.1 MB (81.64%) - LeetHub

--- 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.java
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.java
@@ -1,7 +1,11 @@
 class Solution {
     public int shipWithinDays(int[] weights, int days) {
-        int low = getMax(weights);
-        int high = getSum(weights);
+        int low = Integer.MIN_VALUE;
+        int high = 0;
+        for(int val : weights){
+            low = Math.max(val, low);
+            high += val;
+        }
 
         while(low <= high){
             int mid = low + (high-low) / 2;
@@ -16,24 +20,6 @@ class Solution {
         return low;
     }
 
-    public static int getMax(int[] arr){
-        int max = Integer.MIN_VALUE;
-        for(int i=0;i<arr.length;i++){
-            if(arr[i] > max){
-                max = arr[i];
-            }
-        }
-        return max;
-    }
-
-    public static int getSum(int[] arr){
-        int sum = 0;
-        for(int i=0;i<arr.length;i++){
-            sum += arr[i];
-        }
-        return sum;
-    }
-
     public static int getDays(int[] weights,int mid,int days){
         int day = 1;
         int load = 0;
@@ -41,6 +27,7 @@ class Solution {
             if(load + weights[i] > mid){
                 day++;
                 load = weights[i];
+                if(day > days) return day;
             }else{
                 load += weights[i];
             }
