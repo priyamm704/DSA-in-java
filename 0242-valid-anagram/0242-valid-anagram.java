83fe3fd2c62010fc441d0e119f56a1a58e9357db
priyamm704 <134994495+priyamm704@users.noreply.github.com>
2026-04-09 13:34:27 +0530

    Time: 6 ms (43.4%), Space: 44.2 MB (91.69%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length() != t.length()) return false;
+        int[] hashTable = new int[26];
+
+        for(int i=0;i<s.length();i++){
+            hashTable[s.charAt(i) - 'a']++;
+            hashTable[t.charAt(i) - 'a']--;
+        }
+        for(int val : hashTable){
+            if(val != 0) return false;
+        }
+        return true;
+    }
+}
