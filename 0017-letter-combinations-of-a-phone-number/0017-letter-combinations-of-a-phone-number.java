a13bc3f75f507a4f86f9755d05fa9f40182fd478
priyamm704 <134994495+priyamm704@users.noreply.github.com>
2026-04-30 10:01:05 +0530

    Time: 0 ms (100%), Space: 43.3 MB (88.74%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -1,16 +1,14 @@
 class Solution {
-
-    static final String[] map = {"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
-
     public List<String> letterCombinations(String digits) {
         if(digits == null || digits.length() == 0)return new ArrayList<>();
+        String[] map = {"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
         List<String> list = new ArrayList<>();
         StringBuilder sb = new StringBuilder();
-        helper(0, sb, list, digits);
+        helper(0, sb, list, map, digits);
         return list;
     }
 
-    public void helper(int ind,StringBuilder s, List<String> list, String digits){
+    public void helper(int ind,StringBuilder s, List<String> list, String[] map, String digits){
         if(ind == digits.length()){
             list.add(s.toString());
             return;
@@ -20,7 +18,7 @@ class Solution {
 
         for(int i=0;i<letters.length();i++){
             s.append(letters.charAt(i));
-            helper(ind+1, s, list, digits);
+            helper(ind+1, s, list, map, digits);
             s.deleteCharAt(s.length() - 1);
         }
     }
