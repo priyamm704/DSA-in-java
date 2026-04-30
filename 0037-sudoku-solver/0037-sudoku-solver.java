6a3d81b86d39c3011fc1edaf20b6cd1d0ad8aac0
priyamm704 <134994495+priyamm704@users.noreply.github.com>
2026-04-30 19:36:52 +0530

    Time: 105 ms (92.24%), Space: 42.8 MB (51.94%) - LeetHub

--- 0037-sudoku-solver/0037-sudoku-solver.java
+++ 0037-sudoku-solver/0037-sudoku-solver.java
@@ -1,18 +1,42 @@
 class Solution {
     public void solveSudoku(char[][] board) {
-        solve(board);
+        boolean[][] row = new boolean[9][10];
+        boolean[][] col = new boolean[9][10];
+        boolean[][] box = new boolean[9][10];
+
+        for(int i=0;i<9;i++){
+            for(int j=0;j<9;j++){
+                if(board[i][j] == '.')continue;
+
+                int num = board[i][j] - '0';
+                int boxInd = (i / 3) * 3 + (j / 3);
+
+                row[i][num] = true;
+                col[j][num] = true;
+                box[boxInd][num] = true;
+            }
+        }
+        solveOptimally(board, row, col, box);
     }
 
-    public static boolean solve(char[][] board){
+    public static boolean solveOptimally(char[][] board,boolean[][] row,boolean[][] col,boolean[][] box){
         for(int i=0;i<board.length;i++){
             for(int j=0;j<board[0].length;j++){
                 if(board[i][j] == '.'){
                     for(char c = '1';c<='9';c++){
-                        if(!possible(i, j, c, board))continue;
+                        int num = c - '0';
+                        int b = (i / 3) * 3 + (j / 3);
+                        if(row[i][num] || col[j][num] || box[b][num])continue;
 
                         board[i][j] = c;
-                        if(solve(board))return true;
+                        row[i][num] = true;
+                        col[j][num] = true;
+                        box[b][num] = true;
+                        if(solveOptimally(board, row, col, box))return true;
                         board[i][j] = '.';
+                        row[i][num] = false;
+                        col[j][num] = false;
+                        box[b][num] = false;
                     }
                     return false;
                 }
@@ -21,12 +45,5 @@ class Solution {
         return true;
     }
 
-    public static boolean possible(int row,int col,char c,char[][] board){
-        for(int i=0;i<9;i++){
-            if(board[row][i] == c)return false;
-            if(board[i][col] == c)return false;
-            if(board[3 * (row / 3) + i / 3][3 * (col / 3) + i % 3] == c)return false;
-        }
-        return true;
-    }
+    
 }
