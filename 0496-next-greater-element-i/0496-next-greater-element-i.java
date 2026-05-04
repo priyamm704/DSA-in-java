66b1d11795a21d278c338d94e52f70ff5ca9aaf3
priyamm704 <134994495+priyamm704@users.noreply.github.com>
2026-05-05 04:53:47 +0530

    Time: 4 ms (75.12%), Space: 45.3 MB (35.39%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -1,6 +1,6 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-        Stack<Integer> st = new Stack<>();
+        Deque<Integer> st = new ArrayDeque<>();
         HashMap<Integer, Integer> map = new HashMap<>();
 
         for(int i=nums2.length-1;i>=0;i--){
