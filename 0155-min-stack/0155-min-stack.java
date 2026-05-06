33b6d93f3adc22e663e8bc3c6fcda9b85ddbd85a
priyamm704 <134994495+priyamm704@users.noreply.github.com>
2026-05-06 12:17:07 +0530

    Time: 4 ms (100%), Space: 46.8 MB (83.34%) - LeetHub

--- 0155-min-stack/0155-min-stack.java
+++ 0155-min-stack/0155-min-stack.java
@@ -1,41 +1,56 @@
 class Node{
-    int data;
-    int min;
+    long data;
     Node next;
-    Node(int data,int min,Node next){
+
+    Node(long data){
         this.data = data;
-        this.min = min;
-        this.next = next;
+        this.next = null;
     }
 }
 
 class MinStack {
     Node top;
+    long min;
+
     public MinStack() {
         top = null;
     }
     
     public void push(int val) {
+        Node newNode;
         if(top == null){
-            top = new Node(val, val, null);
+            newNode = new Node(val);
+            min = val;
+        }else if(val < min){
+            long encoded = 2L * val - min;
+            newNode = new Node(encoded);
+            min = val;
         }else{
-            top = new Node(val, Math.min(val, top.min), top);
+            newNode = new Node(val);
         }
+        newNode.next = top;
+        top = newNode;
     }
     
     public void pop() {
         if(top == null) return;
+        if(top.data < min){
+            min = 2 * min - top.data;
+        }
         top = top.next;
     }
     
     public int top() {
         if(top == null) throw new RuntimeException("Stack Is Empty");
-        return top.data;
+        if(top.data < min){
+            return (int)min;
+        }
+        return (int)top.data;
     }
     
     public int getMin() {
         if(top == null) throw new RuntimeException("Stack Is Empty");
-        return top.min;
+        return (int)min;
     }
 }
 
